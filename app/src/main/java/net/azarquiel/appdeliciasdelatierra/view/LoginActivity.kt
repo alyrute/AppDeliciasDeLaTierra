bd83e4c090f17bcdd6743515c2558445a23f3f78
alyrute <117280585+alyrute@users.noreply.github.com>
2024-05-15 13:31:20 +0200

    Login ok

--- app/src/main/java/net/azarquiel/appdeliciasdelatierra/view/LoginActivity.kt
+++ app/src/main/java/net/azarquiel/appdeliciasdelatierra/view/LoginActivity.kt
@@ -17,6 +17,35 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var viewModel: MainViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
 
+        binding.loginButton.setOnClickListener {
+            val emailEditText = binding.email
+            val email = emailEditText.text.toString()
+            val passwordEditText = binding.password
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                viewModel.getLogin(email, password)
+                viewModel.usuario.observe(this, Observer { usuario ->
+                    if (usuario != null) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish() // Finalizar LoginActivity para que no se pueda regresar presionando el botón de atrás
+                    } else {
+                        Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            } else {
+                Toast.makeText(this, "Por favor ingresa email y contraseña", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
 }
